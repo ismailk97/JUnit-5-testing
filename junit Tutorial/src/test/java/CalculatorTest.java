9b2d87ba4dfc6aeffa7a0f73acdc6a8d98793186
ismailk97 <kilinc.i@hotmail.no>
2023-03-10 15:19:37 +0100

    comments

--- junit Tutorial/src/test/java/CalculatorTest.java
+++ junit Tutorial/src/test/java/CalculatorTest.java
@@ -6,10 +6,12 @@ class CalculatorTest {
 
     @Test
     void add() {
-
+        //method is used to check if the actual result of the add method call is equal to the expected result
         assertEquals(4, Calculator.add(2,2));
     }
 
+    
+    /* AssertAll() method tests the multiply() method of the Calculator class with four different input parameters and expected results. */
     @Test
     void multiply() {
         assertAll(() -> assertEquals(4, Calculator.multiply(2, 2)),
